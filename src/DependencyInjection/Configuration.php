25db97c018d4cb07889bb4f14046741cbfa7c5bf
Juan Ardissone <micayael@gmail.com>
2020-10-01 12:29:35 -0400

    Se corrige compatibilidad en la configuración del bundle

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,8 +9,13 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('native_query_from_file_builder');
+        $treeBuilder = new TreeBuilder('native_query_from_file_builder');
+
+        if (\method_exists(TreeBuilder::class, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('native_query_from_file_builder');
+        }
 
         $rootNode
             ->children()
